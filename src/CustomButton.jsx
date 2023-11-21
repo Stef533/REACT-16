180a09754f818349877d0e3b4e95f27e795f72b9
Stefan Petrea <137123241+Stef533@users.noreply.github.com>
2023-11-21 15:50:44 +0100

    Update CustomButton.jsx

--- src/CustomButton.jsx
+++ src/CustomButton.jsx
@@ -1,11 +1,29 @@
-import CustomButton from './CustomButton';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <CustomButton />
-    </div>
-  );
+class CustomButton extends React.Component {
+  handleClickButton = (event) => {
+    if (event.target.tagName !== 'IMG') {
+      console.log('Button clicked:', event.target.name);
+    }
+  }
+
+  handleClickImage = () => {
+    console.log('Image clicked');
+  }
+
+  render() {
+    return (
+      <div className="custom-button">
+        <img
+          ref={(img) => { this.image = img; }}
+          src="https://example.com/your_image_source.jpg"
+          alt="Button Image"
+          onClick={this.handleClickImage} 
+        />
+        <button name="custom-button" onClick={this.handleClickButton}>Click me</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default CustomButton;
